a1b33bb74f80b24c47e28ff3435e54590be42516
Akash Umate <103809994+umateakash@users.noreply.github.com>
2022-04-16 12:26:58 +0530

    This is my practice programs commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, world!</h1>
+      <p>This is my first commit of this project</p>
+    </div>
+  );
+}
+
+export default App;
